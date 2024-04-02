7f7f72b7f7a701e5a196b07bb9472e341d4509a5
Timothi M Abey <timothimabey@gmail.com>
2024-04-02 23:28:34 +0530

    bw

---
+++ blackandwhite.py
@@ -0,0 +1,24 @@
+from images import Image
+def blackAndWhite(image):
+    blackPixel = (0, 0, 0)
+    whitePixel = (225,255,255)
+    for y in range (image.getHeight()):
+        for x in range (image.getWidth()):
+            (r,g,b) = image.getPixel(x,y)
+            average = (r + g + b) // 3
+            if average < 128 :
+                image.setPixel(x, y, blackPixel)
+            else:
+                image.setPixel(x, y, whitePixel)
+
+def main(filename = "timtest.gif"):
+    image = Image(filename)
+    print("Close the image window to continue.")
+    image.draw()
+    blackAndWhite(image)
+    print("Close the image window to quit")
+    image.draw()
+    image.save("bwtim.gif")
+if __name__ == "__main__":
+    main()
+    
